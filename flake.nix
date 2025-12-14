35e5989e83ece9af189ecf6ed1b7414649c22d32
Nikita Zheleztsov <n.zheleztsov@proton.me>
2025-12-14 15:04:35 +0300

    nix: add development shell

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "Nix shell for Tarantool";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system: let
+        pkgs = import nixpkgs { inherit system; };
+    in
+    {
+      devShells.default = pkgs.mkShell {
+        packages = with pkgs; [
+            nodejs
+            python3
+        ];
+        shellHook = ''
+            export LD_LIBRARY_PATH=${pkgs.stdenv.cc.cc.lib}/lib
+            source .venv/bin/activate
+        '';
+
+        # See https://github.com/NixOS/nixpkgs/issues/18995
+        hardeningDisable = [ "fortify" ];
+      };
+    });
+}
